c02bb011ed6d660a027075f0761bdcb74b313a17
Negrustin <negrustin87@gmail.com>
2022-05-25 17:24:11 +0200

    install

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int ballance = 200;
+        int topUpAmmount = 1001;
+        int bonus = 0;
+
+        if (topUpAmmount > 1000){
+            bonus = topUpAmmount/100;
+        }
+        System.out.println(
+                "Ваш начальный балланс "+ ballance +
+                "\nВы пополнили счет на " + topUpAmmount +
+                "\nБонус за пополнение " + bonus +
+                "\nВаш итоговый счет " + (topUpAmmount + bonus));
+    }
+}
